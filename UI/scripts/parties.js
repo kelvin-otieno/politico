3cccca4ffb8e844c32056bb044a78990621f1dcb
kelvin-otieno <kelvinotieno06@gmail.com>
2019-03-01 16:37:01 +0300

    [finishes #164327494] enable editing a party

--- UI/scripts/parties.js
+++ UI/scripts/parties.js
@@ -1,7 +1,7 @@
 
 window.onload = getParties;
 function getParties() {
-    fetch("https://vast-mountain-54945.herokuapp.com/api/v2/parties/", {
+    fetch("http://vast-mountain-54945.herokuapp.com/api/v2/parties/", {
         method: 'GET',
         headers: new Headers({
             'token': localStorage.auth,
@@ -36,9 +36,9 @@ function getParties() {
       <td><img class="party_logo" src="${party.logoUrl}" onerror="if (this.src != './images/party.png') this.src = './images/party.png';"/></td>
       <td>${party.name}</td>
       <td>${party.hqAddress}</td>
-      <td><button id="editButton">Edit</button></td>
+      <td><button id="editButton" type="button" name="${party.party_id}" onclick=getParty(this.name) >Edit</button></td>
       <td>
-        <button id="deleteButton" style="background:#f7919c">Delete</button>
+        <button id="deleteButton" style="background:#f7919c" value="${party.party_id}">Delete</button>
       </td>
     </tr>
         
@@ -54,3 +54,31 @@ function getParties() {
         })
 }
 
+function getID(name) {
+    alert(name)
+}
+
+function getParty(party_id) {
+    fetch("http://vast-mountain-54945.herokuapp.com/api/v2/parties/" + party_id, {
+        method: 'GET',
+        headers: new Headers({
+            'Content-Type': 'application/json',
+            'token': localStorage.auth
+        })
+    })
+        .then((res) => {
+            return res.json()
+        })
+        .then(function (party) {
+
+            localStorage.isEdit = new Boolean(true)
+            localStorage.party_name = party['data'][0]['name']
+            localStorage.hqAddress = party['data'][0]['hqAddress']
+            localStorage.logoUrl = party['data'][0]['logoUrl']
+            localStorage.party_id = party['data'][0]['party_id']
+
+            window.location = "https://kelvin-otieno.github.io/politico/UI/create_party.html"
+
+
+        })
+}
